651d635bb41d5c62ab8ff9b92aa98dd23f4567c9
ogemboeugene <7103.2021@students.ku.ac.ke>
2023-11-14 21:48:42 -0800

    header files

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#endif /*SHELL_H */
