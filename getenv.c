4cd58f188c3357aa641e6f6811bb5d98c865bf7e
ogemboeugene <7103.2021@students.ku.ac.ke>
2023-11-15 00:04:27 -0800

    print environemnt

---
+++ getenv.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+
+/**
+ * Function to print environment variables to stdout
+ * 
+ * @param env: Array of environment variables
+ * 
+ * Return: void
+ */
+
+void _getenv(char **env)
+{
+	size_t i = 0;
+
+	while (env[i])
+	{
+		write(STDOUT_FILENO, env[i], _strlen(env[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
