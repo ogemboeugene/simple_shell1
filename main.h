5bab541f100592b1762fcbaf765a0255a9c6a885
ogemboeugene <7103.2021@students.ku.ac.ke>
2023-11-15 00:53:33 -0800

    headers

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,8 +22,8 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
-char *custom_getline(void);
-char *_read_input(void);
+char *_getline(void);
+char *read_input(void);
 int find_newline(char *buffer, int size);
 char *copy_line(char *buffer, int len, char *line);
 void shift_buffer(char *buffer, int size, int pos);
@@ -32,15 +32,15 @@ char *get_path(char **env);
 int value_path(char **arg, char **env);
 char *_getline_command(void);
 char *read_line(void);
-void print_environment(char **env);
-char **tokenize_input(char *lptr);
+void _getenv(char **env);
+char **tokenize(char *lptr);
 void _exit_command(char **args, char *lineptr, int _exit);
-int fork(char **argc, char **argv, char **env,
+int _fork(char **argc, char **argv, char **env,
 char *lineptr, int np, int c);
-int _set_environment_variable(char **args);
-int _unset_environment_variable(char **args);
+int _setenv(char **args);
+int _unsetenv(char **args);
 void handle_setenv_command(char **args);
 void handle_unsetenv_command(char **args);
 
 
-#endif /* MAIN_H */
+#endif /* SHELL_H */
